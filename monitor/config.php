1e5a1d9edef3e4111bd2088c94c03f22b5ce3cbd
Mostalk <ivdifa.ka@gmail.com>
2018-09-26 01:32:32 +0300

    Fix for Linux

--- monitor/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('PSM_DB_HOST', 'localhost');
-define('PSM_DB_PORT', '');
-define('PSM_DB_NAME', 'monitor');
-define('PSM_DB_USER', 'admin');
-define('PSM_DB_PASS', 'qpwoeirutY');
-define('PSM_DB_PREFIX', 'psm_');
-define('PSM_BASE_URL', 'http://109.254.211.5/monitor');
