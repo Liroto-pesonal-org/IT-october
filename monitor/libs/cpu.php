6e4df1d493dd41cb17ee2958f4e1bba5bed1e9c0
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 14:33:54 +0300

    Back

--- monitor/libs/cpu.php
+++ monitor/libs/cpu.php
@@ -75,15 +75,8 @@ if ($Config->get('cpu:enable_temperature'))
         {
             $temp = round($t[0] / 1000).' °C';
         }
-    }?>
-	<script>
-	 if($(".temp")>= 10) clearInterval(interval);
-                $(".temp").find(".container")
-                          .css("color", "red");
-            });    
-	</script>
-}<?
-
+    }
+}
 
 $datas = array(
     'model'      => $model,
