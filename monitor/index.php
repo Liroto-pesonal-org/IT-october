01add7284a86c5ee9a002cfbc03a772c53d2ea1e
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 14:41:31 +0300

    Test

--- monitor/index.php
+++ monitor/index.php
@@ -56,7 +56,11 @@ $update = $Config->checkUpdate();
             echo Misc::getHostname().' - '.Misc::getLanIP();
         ?>
     </div>
-
+	<div id="killx">
+	<?php
+	echo $Config->get('esm:custom_title');
+	?>
+	</div>
     <?php if (!is_null($update)): ?>
         <div id="update">
             <a href="<?php echo $update['fullpath']; ?>">New version available (<?php echo $update['availableVersion']; ?>) ! Click here to download</a>
