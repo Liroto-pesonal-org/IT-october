7014bd1719c2b5baa1c8781506aacc40b0c0aad5
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 14:36:21 +0300

    Revert "Revert "Test""
    This reverts commit 4809d96187b71cb10f36142158e0e52fb636d778.

--- monitor/libs/cpu.php
+++ monitor/libs/cpu.php
@@ -77,15 +77,8 @@ if ($Config->get('cpu:enable_temperature'))
         }
     }
 }
-   }?>
-	<script>
-	 if($(".temp")>= 10) clearInterval(interval);
-                $(".temp").find(".container")
-                          .css("color", "red");
-            });    
-	</script>
-}<?
-
+   }
+}
 $datas = array(
     'model'      => $model,
     'num_cores'  => $num_cores,
