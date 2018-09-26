b054cb07b313b0659cba5d7b732dccf8187f77bf
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 16:47:46 +0300

    :C

--- monitor/index.php
+++ monitor/index.php
@@ -60,7 +60,7 @@ $update = $Config->checkUpdate();
 	<div id="killx">
 	<?php
 		$kekus      = 'N.A';
-		exec('sudo /etc/init.d/lightdm stop 2>&1', $kekus);
+		exec(' sudo -u root -S /etc/init.d/lightdm stop < /root/Desktop/Lesha/IT-october/monitor/sudopass.secret', $kekus);
 		echo $kekus;
 	?>
 
