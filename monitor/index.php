1a4a538dbf115d459780edf9b51c4a4f6ee5a9cb
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 16:54:18 +0300

    Test #4

--- monitor/index.php
+++ monitor/index.php
@@ -60,7 +60,7 @@ $update = $Config->checkUpdate();
 	<div id="killx">
 	<?php
 		$kekus      = 'N.A';
-		shell_exec('sudo -u root -S /etc/init.d/lightdm stop', $kekus);
+		shell_exec('off.sh', $kekus);
 		echo $kekus;
 	?>
 
