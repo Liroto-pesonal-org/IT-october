1baae260f315ea7c0d24024e32e3e53f54564bc0
Ivan Shevchenko <ivdifa.ka@gmail.com>
2018-09-26 05:11:44 +0300

    Optimize

--- monitor/index.php
+++ monitor/index.php
@@ -126,7 +126,7 @@ $update = $Config->checkUpdate();
         <div class="box-content t-center">
             <div class="f-left w33p">
                 <h3>1 min</h3>
-                <input type="text" class="gauge" id="load-average_1" value="0" data-height="100" data-width="250" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
+                <input type="text" class="gauge" id="load-average_1" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
             </div>
 
             <div class="f-right w33p">
